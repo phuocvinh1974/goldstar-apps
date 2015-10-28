068f400d635ba5d10ada767c53c2498fc7c9e6d3
nguyenhuuhanhlam <nguyenhuuhanhlam@gmail.com>
2015-10-28 22:56:18 +0700

    start...

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>GS APPS</title>
+	<link rel="stylesheet" href="default.css">
+</head>
+<body>
+	<div style="margin:12px 0 0 12px">
+		<div style="margin:0 0 12px 0">APPS FOLDER</div>
+		<?php
+			foreach ( glob ('./*', GLOB_ONLYDIR) as $dir )
+			{
+			    echo
+			    	'<div style="margin-bottom:3px">
+			    	<li><a href="/' . basename ($dir) . '" style=" letter-spacing:3px">' . basename ($dir) . '</a></li>
+			    	</div>';
+			}
+		?>
+	</div>
+</body>
+</html>
