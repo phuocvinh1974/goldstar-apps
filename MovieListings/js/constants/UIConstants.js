d71f1179a1984fbd1c3b4051235d9660e7f6fd10
nguyenhuuhanhlam <nguyenhuuhanhlam@gmail.com>
2015-11-05 08:58:16 +0700

    ...

--- MovieListings/js/constants/UIConstants.js
+++ MovieListings/js/constants/UIConstants.js
@@ -1,6 +1,11 @@
 var keyMirror = require('keymirror');
 
 module.exports = keyMirror({
-	SHOW_DRAWER: null, HIDE_DRAWER: null,
-	SHOW_MORE_MENU: null, HIDE_MORE_MENU: null
+	SHOW_DRAWER: null,
+	HIDE_DRAWER: null,
+	
+	SHOW_MORE_MENU: null,
+	HIDE_MORE_MENU: null,
+	
+	SHOW_TOPBAR_QUICK_SEARCH:null
 });
